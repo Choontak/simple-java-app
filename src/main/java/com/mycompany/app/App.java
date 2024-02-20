9825855ba3545521f881c94e9332948390134988
Chan Choon Tak <bia.boa.tester@gmail.com>
2024-02-20 16:34:15 +0800

    Restore the code wihout error.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Tried to push to GitHub, it works!");
+        System.out.println("Schedule testing!");
     }
 
     public String getMessage() {
