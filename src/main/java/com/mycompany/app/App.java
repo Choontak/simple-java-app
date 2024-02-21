34894bbeac822ff7b9d43771943584051b377c3c
Chan Choon Tak <bia.boa.tester@gmail.com>
2024-02-21 09:24:16 +0800

    Web Hook Test.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Schedule testing!");
+        System.out.println("web hook test!");
     }
 
     public String getMessage() {
