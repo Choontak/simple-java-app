9c5959a093dced858821f747cabc69c8533f55a0
Chan Choon Tak <bia.boa.tester@gmail.com>
2024-02-21 14:13:47 +0800

    Pipeline Test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Attending CI/CD Training");
+        System.out.println("Attending CI/CD Training - web hook");
     }
 
     public String getMessage() {
