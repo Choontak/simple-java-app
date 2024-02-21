a55a88dd14fbad35aef2d6044b732517baf01f2d
Chan Choon Tak <bia.boa.tester@gmail.com>
2024-02-21 11:02:59 +0800

    Testing out email

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("web hook test now!");
+        System.out.println("test email notification");
     }
 
     public String getMessage() {
